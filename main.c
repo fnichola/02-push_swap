627ccec8f2171287d424c13f49ea9858ec3f4952
Flenghi Nicholas foppiano <fnichola@student.42tokyo.jp>
2021-12-07 18:09:38 +0900

    add dlist structure and basic functions to fill stack a

--- main.c
+++ main.c
@@ -6,22 +6,79 @@
 /*   By: fnichola <fnichola@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/06 16:26:10 by fnichola          #+#    #+#             */
-/*   Updated: 2021/12/06 23:16:47 by fnichola         ###   ########.fr       */
+/*   Updated: 2021/12/07 18:09:03 by fnichola         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 #include <stdio.h>
 
-int	main(void)
+t_dlist	*ft_dlstnew(int value)
 {
-	t_list	*a;
-	// t_list	*b;
+	t_dlist	*new_dlist;
 
-	a = ft_lstnew((int *)malloc(sizeof(int *)));
-	int	x = 42;
-	a->content = &x;
-	printf("%d\n", *(int *)a->content);
+	new_dlist = malloc(sizeof(t_dlist));
+	if (!new_dlist)
+		return (NULL);
+	new_dlist->prev = NULL;
+	new_dlist->next = NULL;
+	new_dlist->value = value;
+	return (new_dlist);
+}
 
+t_dlist	*ft_dlstlast(t_dlist *lst)
+{
+	if (!lst)
+		return (NULL);
+	while (lst->next)
+		lst = lst->next;
+	return (lst);
+}
+
+t_dlist	*ft_dlstfirst(t_dlist *lst)
+{
+	if (!lst)
+		return (NULL);
+	while (lst->prev)
+		lst = lst->prev;
+	return (lst);
+}
+
+/**
+ * Adds the element ’new’ at the end of the list.
+*/
+void	ft_dlstadd_back(t_dlist **lst, t_dlist *new)
+{
+	t_dlist	*lst_last;
+
+	if (*lst == NULL)
+	{
+		*lst = new;
+		return ;
+	}
+	lst_last = ft_dlstlast(*lst);
+	lst_last->next = new;
+	new->prev = lst_last;
+}
+
+int	main(int argc, char **argv)
+{
+	int		i;
+	t_dlist	*a;
+
+	a = NULL;
+	i = 1;
+	while (i < argc)
+	{
+		ft_dlstadd_back(&a, ft_dlstnew(ft_atoi(argv[i])));
+		i++;
+	}
+	a = ft_dlstfirst(a);
+	printf("a:\n");
+	while (a)
+	{
+		printf("%d\n", a->value);
+		a = a->next;
+	}
 	return (0);
 }
--- push_swap.h
+++ push_swap.h
@@ -6,10 +6,14 @@
 /*   By: fnichola <fnichola@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/06 22:29:14 by fnichola          #+#    #+#             */
-/*   Updated: 2021/12/06 22:30:41 by fnichola         ###   ########.fr       */
+/*   Updated: 2021/12/07 16:59:35 by fnichola         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft/libft.h"
 
-
+typedef struct	s_dlist {
+	struct s_dlist	*prev;
+	int			value;
+	struct s_dlist	*next;
+}				t_dlist;
