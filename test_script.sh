d874365e5815cc6858454ec1a788cca115373b57
Nic Flenghi <nic.flenghi@gmail.com>
2022-01-05 23:56:43 +0900

    update test script to average multiple tests and check for errors

--- test_script.sh
+++ test_script.sh
@@ -1,5 +1,36 @@
 #!/bin/bash
 #ARG=`ruby -e "puts (1..5).to_a.shuffle.join(' ')"`; ./push_swap $(echo $ARG)
-ARG=`ruby -e "puts (1..25).to_a.shuffle.join(' ')"`; echo $ARG; ./push_swap $(echo $ARG)
+#ARG=`ruby -e "puts (1..25).to_a.shuffle.join(' ')"`; ./push_swap $(echo $ARG)
 #ARG=`ruby -e "puts (1..100).to_a.shuffle.join(' ')"`; ./push_swap $(echo $ARG)
 #ARG=`ruby -e "puts (1..500).to_a.shuffle.join(' ')"`; ./push_swap $(echo $ARG)
+
+total=0
+max=0
+failed=0
+for i in `seq 1 100`
+do
+	ARG=`ruby -e "puts (1..500).to_a.shuffle.join(' ')"`
+	count=$(./push_swap $(echo $ARG) | wc -l)
+	checker_result=$(./push_swap $(echo $ARG) | ./checker_linux $(echo $ARG))
+
+	if [ "$checker_result" = "KO" ]
+	then
+		failed=1
+	fi
+
+	total=$(($total + $count))
+	echo "$i: $count" 
+	echo $checker_result
+
+	if [ $count -gt $max ]
+	then
+		max=$count
+	fi
+done
+average=$(($total/100))
+echo "Average = $average"
+echo "Max = $max"
+if [ $failed = 1 ]
+then
+	echo "SORTING ERROR!!!"
+fi
